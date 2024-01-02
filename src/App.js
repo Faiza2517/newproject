c82ee53474b84bfb3b609ec89b9471a2966da572
Faiza <rfaiza340@gmail.com>
2024-01-02 12:34:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+ppppppppppppppppppppppppp        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
