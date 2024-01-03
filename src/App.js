39aa52f9dcdcfc994d1d78d876c4335d27d55018
Faiza <rfaiza340@gmail.com>
2024-01-03 10:43:28 +0500

    chng code

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-ppppppppppppppppppppppppp        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={logo} className="App-logo" alt="logo" />   
+       <h1>faiza</h1>
+       <h2>Hello</h2>
+    </header>
     </div>
   );
 }
